2eb427147099ab738e2b7b5d672e8ad63d9545df
mesuit <makatech92@gmail.com>
2025-09-23 03:14:03 -0700

    Create authController.js

---
+++ controllers/authController.js
@@ -0,0 +1,39 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const generateToken = (id) => jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+
+export const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const exists = await User.findOne({ email });
+    if (exists) return res.status(400).json({ message: "User already exists" });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashed = await bcrypt.hash(password, salt);
+
+    const user = await User.create({ name, email, password: hashed });
+    res.json({ _id: user.id, name: user.name, email: user.email, token: generateToken(user.id) });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      res.json({ _id: user.id, name: user.name, email: user.email, token: generateToken(user.id) });
+    } else {
+      res.status(400).json({ message: "Invalid credentials" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+export const getProfile = async (req, res) => {
+  res.json(req.user);
+};
