815445535350045e303862e70bac9d131de32f93
mesuit <makatech92@gmail.com>
2025-09-23 04:44:06 -0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,8 +1,12 @@
+// config/db.js
 import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`❌ Error: ${error.message}`);
